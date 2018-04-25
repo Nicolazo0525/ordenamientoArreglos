1926066ab804bcdac21f3669c37886cceb3cf9f5
USUARIO <USUARIO@PF16>
2018-04-24 19:31:39 -0500

    Ordenamiento de objetos

---
+++ src/estudiante/Estudiante.java
@@ -0,0 +1,52 @@
+package estudiante;
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+    private int puesto;
+    private double nota;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void setPuesto(int puesto) {
+        this.puesto = puesto;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public int getPuesto() {
+        return puesto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+    public String imprimir(){
+        return ""; 
+    }
+    
+}
